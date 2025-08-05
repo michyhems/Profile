6dd260ea23bfb1f26a690ae819946ecff7175e43
michyhems <64618905+michyhems@users.noreply.github.com>
2025-08-05 23:37:48 +1000

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,16 +1,32 @@
 import { useState, useEffect } from "react";
-import { projectData } from "./projectData";
+import axios from "./../api/axios";
 
 const Projects = () => {
     const [projectList, setProjectList] = useState([]);
 
-    //declare imported data as a state variable
+    //query backend and set project list.
     useEffect(() => {
         let newProject = [];
-        projectData.map((data) => {
-            newProject.push(data);
-        });
-        setProjectList(newProject);
+        let projectData;
+        const controller = new AbortController();
+        const getProjects = async () => {
+            try {
+                const response = await axios.get(`/api`, {
+                    signal: controller.signal,
+                });
+                projectData = response.data;
+                projectData.map((data) => {
+                    newProject.push(data);
+                });
+                setProjectList(newProject);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        getProjects();
+        return () => {
+            controller.abort();
+        };
     }, []);
 
     return (
@@ -18,51 +34,73 @@ const Projects = () => {
             <section>
                 {/**Looping over each data entry and generating a list item from it. */}
                 <ul class="projectList">
-                    {projectList.map((proj) => (
-                        //list item container
-                        <li class="listItem">
-                            {/**Project thumbnail */}
-                            <img class="thumbnail" src={proj.thumbnail}></img>
+                    {projectList?.length ? (
+                        projectList.map((proj) => (
+                            //list item container
+                            <li class="listItem">
+                                {/**Project thumbnail */}
+                                <img
+                                    class="thumbnail"
+                                    src={proj.imageSource}
+                                    alt="None"
+                                ></img>
 
-                            {/**Project description and links */}
-                            <div class="content">
-                                <h2>{proj.title}</h2>
-                                <p>{proj.description}</p>
+                                {/**Project description and links */}
+                                <div class="content">
+                                    <h2>{proj.title}</h2>
+                                    <p>{proj.description}</p>
 
-                                {/**Project Links */}
-                                <a href={proj.source} target="_blank">
-                                    <p class="seeMore">
-                                        Click here to view project.
-                                    </p>
-                                </a>
-
-                                {/**Not all projects have a GitHub repository and so a turnary statement is used to display the link
-                                 * only if it exists.
-                                 */}
-                                {proj.gitRepo !== "" ? (
-                                    <a href={proj.gitRepo} target="_blank">
-                                        <p>
-                                            Click here to see source code in
-                                            GitHub.
+                                    {/**Project Links */}
+                                    <a href={proj.source} target="_blank">
+                                        <p class="seeMore">
+                                            Click here to view project.
                                         </p>
                                     </a>
-                                ) : null}
+
+                                    {/**Not all projects have a GitHub repository and so a turnary statement is used to display the link
+                                     * only if it exists.
+                                     */}
+                                    {proj.gitRepo !== "" ? (
+                                        <a href={proj.gitRepo} target="_blank">
+                                            <p>
+                                                Click here to see source code in
+                                                GitHub.
+                                            </p>
+                                        </a>
+                                    ) : null}
+                                </div>
+                            </li>
+                        ))
+                    ) : (
+                        //unfortunately render.com's free tier can have a long loading time when there is a period of inactivity so a loading banner is necessary
+                        <section class="waitMessage">
+                            <h3>
+                                Please wait as we fetch your projects, this
+                                could take a minute :{")"}
+                            </h3>
+                            {/**Loading dots */}
+                            <div class="loading">
+                                <div></div>
+                                <div></div>
+                                <div></div>
                             </div>
-                        </li>
-                    ))}
+                        </section>
+                    )}
                 </ul>
 
                 {/**Image reference */}
-                <p>
-                    Profile image by{" "}
-                    <a
-                        style={{ color: "blue" }}
-                        href="https://www.flaticon.com/authors/md-tanvirul-haque"
-                        target="_blank"
-                    >
-                        Md Tanvirul Haque
-                    </a>
-                </p>
+                {projectList?.length ? (
+                    <p>
+                        Profile image by{" "}
+                        <a
+                            style={{ color: "blue" }}
+                            href="https://www.flaticon.com/authors/md-tanvirul-haque"
+                            target="_blank"
+                        >
+                            Md Tanvirul Haque
+                        </a>
+                    </p>
+                ) : null}
             </section>
         </>
     );
